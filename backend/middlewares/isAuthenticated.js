9dac1f2d152107b9292800301f6bf4114403bd56
mtthw <113967536+abfasb@users.noreply.github.com>
2025-01-08 20:12:37 +0800

    add isNotAuthenticated for middleware

--- backend/middlewares/isAuthenticated.js
+++ backend/middlewares/isAuthenticated.js
@@ -22,4 +22,21 @@ const isAuthenticated = async (req, res, next) => {
         console.log(error);
     }
 }
+
+const isNotAuthenticated = async (req, res, next) =>{
+      try {
+        const token = req.cookies.token;
+        if (token) {
+            return res.status(200).json({
+                message: "User authenticated",
+                success: true,
+            })
+        }
+        next();
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
 export default isAuthenticated;
